78f34a0c2da46e87942d0d6f6f7cc6905672e091
PavithraRanjana <dranjana56@gmail.com>
2024-06-26 17:13:10 +0530

    beer advisor app completed

--- app/src/main/java/com/example/beeradviser/MainActivity.kt
+++ app/src/main/java/com/example/beeradviser/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.beeradviser
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,9 +20,30 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // getting reference to the views
         val findBeer = findViewById<Button>(R.id.find_beer)
+        val brands = findViewById<TextView>(R.id.brands)
+        val beerColor = findViewById<Spinner>(R.id.beer_color)
+
+        // beer colors map
+        val beerBrands = mapOf(
+            "Light" to listOf("Pilsner Urquell", "Budweiser", "Heineken", "Miller Lite"),
+            "Amber" to listOf("Alaskan Amber", "Stone Levitation Ale", "Bell’s Amber Ale", "at Tire Amber Ale"),
+            "Brown" to listOf("Newcastle Brown Ale", "Samuel Smith's Nut Brown Ale", "Brooklyn Brown Ale", "Dogfish Head Indian Brown Ale"),
+            "Dark" to listOf("Guinness Draught", "Samuel Adams Boston Lager", "Sierra Nevada Porter", "Founders Porter")
+        )
+
+        // lambda to create a new string with \n character
+        val finalString: (x:List<String>) -> String = {it.joinToString(separator = "\n")}
+
+        // method that will return a list of beers based on selected color
+        fun getBeers(selectedColor: String) {
+            brands.text = beerBrands[selectedColor]?.let { finalString(it) }
+        }
+
         findBeer.setOnClickListener {
-            
+            val color = beerColor.selectedItem.toString() // getting the current value selected in the spinner
+            getBeers(color)
         }
     }
 }
